5717c9ccc40adee7b871ccc047f845a130ae8cde
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2020-11-13 15:58:05 +0700

    Initial commit Make a stable game FPS and UPS Need more than 1 hour just to make this Hell

---
+++ app/src/main/java/com/example/wpg3kelompok9/Game.java
@@ -0,0 +1,77 @@
+package com.example.wpg3kelompok9;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Game manages all objects in the game and it responsible for
+ * updating all states and render all object to the screen
+ */
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+
+    private GameLoop gameLoop;
+    private Context context;
+
+    // The Constructor
+    public Game(Context context) {
+        super(context);
+
+        // Get surface holder and add callback
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        this.context = context;
+
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas) {
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        paint.setColor(Color.argb(255,  255, 0, 0));
+        paint.setTextSize(50);
+        canvas.drawText("UPS : " + averageUPS,100,100,paint);
+    }
+
+    public void drawFPS(Canvas canvas) {
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        paint.setColor(Color.argb(255,  255, 0, 0));
+        paint.setTextSize(50);
+        canvas.drawText("FPS : " + averageFPS,100,200,paint);
+    }
+
+    public void update() {
+        //Update game state
+    }
+}
---
+++ app/src/main/java/com/example/wpg3kelompok9/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.wpg3kelompok9;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set window to fullscreen (will hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game, so that object in
+        // The game class can be rendered to the screen
+        setContentView(new Game(this));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
