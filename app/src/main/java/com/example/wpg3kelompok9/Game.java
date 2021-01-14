9fd8d7583077eaf6270c9fa3a48b648570b0acab
Alifianrc <62532983+Alifianrc@users.noreply.github.com>
2021-01-15 04:06:46 +0700

    Edit some little detail

--- app/src/main/java/com/example/wpg3kelompok9/Game.java
+++ app/src/main/java/com/example/wpg3kelompok9/Game.java
@@ -7,7 +7,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.media.AudioManager;
